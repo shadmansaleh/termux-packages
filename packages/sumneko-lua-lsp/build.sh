6eeacc4fd936b8032084094daefd33cead6836f6
shadmansaleh <shadmansaleh3@gmail.com>
2021-06-24 00:02:20 +0600

    try6

--- packages/sumneko-lua-lsp/build.sh
+++ packages/sumneko-lua-lsp/build.sh
@@ -4,20 +4,24 @@ TERMUX_PKG_LICENSE="MIT"
 TERMUX_PKG_MAINTAINER="@shadmansaleh"
 TERMUX_PKG_VERSION=2.0.1
 TERMUX_PKG_SRCURL=https://github.com/sumneko/lua-language-server.git
-TERMUX_PKG_GIT_BRANCH="master"
 TERMUX_PKG_BUILD_DEPENDS="ninja"
 TERMUX_PKG_HOSTBUILD=true
 
 termux_step_host_build() {
-	cd $TERMUX_PKG_SRCDIR
+	mkdir -p $TERMUX_PKG_HOSTBUILD_DIR/build
+	cd $TERMUX_PKG_HOSTBUILD_DIR/build
+  git clone --depth 1 https://github.com/sumneko/lua-language-server.git
+  cd lua-language-server
+  git 
+  cd lua-language-server
+  git submodule update --recommend-shallow --init --recursive
   cd 3rd/luamake
   ./compile/install.sh
   cd ../..
   ./3rd/luamake/luamake rebuild
-  tar -zcf ../sumneko-lua-lsp.tar.gz .
-  mv ../sumneko-lua-lsp.tar.gz .
+  tar -zcf $TERMUX_PKG_HOSTBUILD_DIR/build/sumneko-lua-lsp.tar.gz .
 }
 
 termux_step_make_install() {
-	install -Dm600 sumneko-lua-lsp.tar.gz "$TERMUX_PREFIX"/share/sumneko-lua-lsp/sumneko-lua-lsp.tar.gz
+	install -Dm600 $TERMUX_PKG_HOSTBUILD_DIR/build/sumneko-lua-lsp.tar.gz "$TERMUX_PREFIX"/share/sumneko-lua-lsp/sumneko-lua-lsp.tar.gz
 }
