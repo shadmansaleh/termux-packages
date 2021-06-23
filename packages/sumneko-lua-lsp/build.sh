dd845f54b8368669fa34ef915df5e4dde1d4f7c3
shadmansaleh <shadmansaleh3@gmail.com>
2021-06-23 22:11:56 +0600

    try1

---
+++ packages/sumneko-lua-lsp/build.sh
@@ -0,0 +1,22 @@
+TERMUX_PKG_HOMEPAGE=http://www.inf.puc-rio.br/~roberto/lpeg
+TERMUX_PKG_DESCRIPTION="Lsp server for lua"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@shadmansaleh"
+TERMUX_PKG_VERSION="master"
+TERMUX_PKG_SRCURL=https://github.com/sumneko/lua-language-server.git
+TERMUX_PKG_GIT_BRANCH=$TERMUX_PKG_VERSION
+TERMUX_PKG_BUILD_DEPENDS="ninja"
+TERMUX_PKG_BUILD_IN_SRC=true
+
+termux_step_make() {
+  git submodule update --recommend-shallow --init --recursive
+  cd 3rd/luamake
+  ./compile/install.sh
+  cd ../..
+  ./3rd/luamake/luamake rebuild
+  tar -zvcf ./sumneko-lua-lsp.tar.gz .
+}
+
+termux_step_make_install() {
+	install -Dm600 sumneko-lua-lsp.tar.gz "$TERMUX_PREFIX"/share/sumneko-lua-lsp/sumneko-lua-lsp.tar.gz
+}
