a597c2c1b238eada63f55deafba7e6dd287d9049
shadmansaleh <shadmansaleh3@gmail.com>
2021-07-06 19:31:06 +0600

    T2

--- packages/luamake/build.sh
+++ packages/luamake/build.sh
@@ -5,11 +5,11 @@ TERMUX_PKG_MAINTAINER="@shadmansaleh"
 TERMUX_PKG_VERSION=1.0.0
 TERMUX_PKG_SRCURL=https://github.com/actboy168/luamake.git
 TERMUX_PKG_GIT_BRANCH="master"
-TERMUX_PKG_BUILD_DEPENDS="ninja"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 
 termux_step_make() {
+  termux_setup_ninja
   cd $TERMUX_PKG_SRCDIR
   ./compile/install.sh
 }
