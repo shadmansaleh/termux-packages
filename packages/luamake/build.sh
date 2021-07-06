d85d4876f8e6e37ebc125fbae31e826c1188a499
shadmansaleh <shadmansaleh3@gmail.com>
2021-07-06 19:02:21 +0600

    Build luamake

---
+++ packages/luamake/build.sh
@@ -0,0 +1,19 @@
+TERMUX_PKG_HOMEPAGE=https://github.com/actboy168/luamake
+TERMUX_PKG_DESCRIPTION="Build tool in lua"
+TERMUX_PKG_LICENSE="MIT"
+TERMUX_PKG_MAINTAINER="@shadmansaleh"
+TERMUX_PKG_VERSION=1.0.0
+TERMUX_PKG_SRCURL=https://github.com/actboy168/luamake.git
+TERMUX_PKG_GIT_BRANCH="master"
+TERMUX_PKG_BUILD_DEPENDS="ninja"
+TERMUX_PKG_BUILD_IN_SRC=true
+
+
+termux_step_make() {
+  cd $TERMUX_PKG_SRCDIR
+  ./compile/install.sh
+}
+
+termux_step_make_install() {
+	install -Dm700 $TERMUX_PKG_SRCDIR/luamake "$TERMUX_PREFIX"/bin/luamake
+}
